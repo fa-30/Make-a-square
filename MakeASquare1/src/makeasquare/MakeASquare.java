53374509b907e566c8a21fbcd68d06f807d0fe7f
fa-30 <105087280+fa-30@users.noreply.github.com>
2023-12-16 14:14:09 +0200

    Add files via upload

---
+++ MakeASquare1/src/makeasquare/MakeASquare.java
@@ -0,0 +1,21 @@
+
+package makeasquare;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+
+public class MakeASquare {
+    
+    public static void main(String[] args)  {
+        openForm(new InputPieces());
+        
+    }
+
+    // open any form
+        public static void openForm(JFrame form) {
+            form.setVisible(true);
+    }
+
+}
